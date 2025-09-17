fd238288620de5ac1b15731e862d873c940de8f8
MondaSamuel <samwelmonda48@gmail.com>
2025-09-17 05:44:39 +0300

    fixed some https

--- src/views/differs.vue
+++ src/views/differs.vue
@@ -447,7 +447,7 @@ export default {
   name: 'DigitDiffersTradingDashboard',
   setup() {
     // API Configuration
-    const API_BASE_URL = 'codexd-production.up.railway.app'
+    const API_BASE_URL = 'https://codexd-production.up.railway.app'
 
     // Configuration state
     const config = ref({
